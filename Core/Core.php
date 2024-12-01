3a0b53636b8bce81abb2f9c80e850af1476fe48f
luniak <jedrzej.flak+github@gmail.com>
2024-12-02 00:56:58 +0100

    Core fix

--- Core/Core.php
+++ Core/Core.php
@@ -3,10 +3,21 @@ namespace Core;
 
 class Core
 {
-    public function __construct(string $mode = 'test', int $day = 1, int $part = 1, ... $params)
+    public function __construct(string $mode = 'help', int $day = 1, int $part = 1, ... $params)
     {
-        $class = "Answers\Day$day";
-        $t = new $class($mode);
-        $t->{'part' . $part}();
+        if ('help' === $mode) {
+            echo
+                "Help:\n\r"
+                . "Params in order:\n\r"
+                . "mode: test/main\n\r"
+                . "day: 1-24\n\r"
+                . "part: 1-2\n\r"
+            ;
+
+        } else {
+            $class = "Answers\Day$day";
+            $t = new $class($mode);
+            $t->{'part' . $part}();
+        }
     }
 }
